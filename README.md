341fb6aebbe163238526d77cc2fe13023670af6d
Panttersson <Gabrielhas2001@gmail.com>
2023-03-01 18:58:08 -0300

    correcao de palavra

--- README.md
+++ README.md
@@ -9,5 +9,5 @@ Aplicação que pega informações de cidades, sendo elas o clima, o país, a fo
 Weather Map Api - https://openweathermap.org/current 
 (Para usar sua forma gratuita, basta criar uma conta e acessar sua chave API em seu perfil)
 <br>
-Country Flags Api - https://countryflagsapi.com/ 
+Country Flags Api - https://flagsapi.com
 (Essa Api não tem Key, então é bem mais facil de utilizar)
--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 const apiKey = "0c18adb50ce7c59a05af758744753aad";
-const apiCountryURL = "https://countryflagsapi.com/png";
 
 const cityInput = document.querySelector('#city-input');
 const searchBtn = document.querySelector('#search');
@@ -9,7 +8,7 @@ const tempElement = document.querySelector('#temperature span');
 const descElement = document.querySelector('#description');
 const weatherIconElement = document.querySelector('#weather-icon');
 const countryElement = document.querySelector('#country');
-const umidityElement = document.querySelector('#umidity span');
+const humidityElement = document.querySelector('#humidity span');
 const windElement = document.querySelector('#wind span');
 
 const getWeatherData = async (city) => {
@@ -17,12 +16,19 @@ const getWeatherData = async (city) => {
 
     const res = await fetch(apiWeatherURL);
     const data = await res.json();
-
-    console.log(data)
+    return data
 };
 
-const showWeatherData = (city) => {
-    getWeatherData(city);
+const showWeatherData = async (city) => {
+    const data = await getWeatherData(city);
+
+    cityElement.innerText = data.name;
+    tempElement.innerText = parseInt(data.main.temp);
+    descElement.innerText = data.weather[0].description
+    weatherIconElement.setAttribute('src', `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`);
+    countryElement.setAttribute("src", `https://flagsapi.com/${data.sys.country}/flat/64.png`);
+    humidityElement.innerText = `${data.main.humidity}%`;
+    windElement.innerText = `${data.wind.speed}km/h`;
 };
 
 searchBtn.addEventListener('click', (e) => {
