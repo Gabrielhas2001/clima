0542d5a1f7a575d5a2dcdf6b50d86cdde5eeecfe
Panttersson <Gabrielhas2001@gmail.com>
2023-03-01 18:33:27 -0300

    inicializando api

--- js/script.js
+++ js/script.js
@@ -1,1 +1,34 @@
-// 0c18adb50ce7c59a05af758744753aad
+const apiKey = "0c18adb50ce7c59a05af758744753aad";
+const apiCountryURL = "https://countryflagsapi.com/png";
+
+const cityInput = document.querySelector('#city-input');
+const searchBtn = document.querySelector('#search');
+
+const cityElement = document.querySelector('#city');
+const tempElement = document.querySelector('#temperature span');
+const descElement = document.querySelector('#description');
+const weatherIconElement = document.querySelector('#weather-icon');
+const countryElement = document.querySelector('#country');
+const umidityElement = document.querySelector('#umidity span');
+const windElement = document.querySelector('#wind span');
+
+const getWeatherData = async (city) => {
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+
+    const res = await fetch(apiWeatherURL);
+    const data = await res.json();
+
+    console.log(data)
+};
+
+const showWeatherData = (city) => {
+    getWeatherData(city);
+};
+
+searchBtn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    const city = cityInput.value;
+
+    showWeatherData(city)
+});
